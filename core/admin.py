81c8f1b013757a99a95c5e7c00a84d1fe8a87e2a
unknown <quadfolius@gmail.com>
2008-10-20 21:00:05 +0800

    pholius commit 1

---
+++ core/admin.py
@@ -0,0 +1,5 @@
+from pholius.core import models
+from django.contrib import admin
+
+admin.site.register(models.Taxonomy)
+admin.site.register(models.Content)
---
+++ core/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Taxonomy(models.Model):
+    name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=32)
+    type = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+	    verbose_name_plural = 'taxonomies'
+
+class Content(models.Model):
+    title = models.CharField(max_length=160)
+    slug = models.SlugField(max_length=80, unique_for_date='created')
+    author = models.ForeignKey(User)
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
+    taxonomy = models.ManyToManyField(Taxonomy, blank=True)
+    #response_count = models.SmallIntegerField(maxlength=4)
+    #allow_comments = models.BooleanField()
+    #allow_pings = models.BooleanField()
+
+    def __unicode__(self):
+        return self.title    
