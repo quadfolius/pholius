88879281d78205e7582f17b09e5066129b0a0103
Kaid Wong <quadfolius@gmail.com>
2008-10-21 11:29:15 +0800

    small modification to ContentAdmin class

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,13 @@
 from pholius.core import models
 from django.contrib import admin
 
+class ContentAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Content header',  {'fields': ['title', 'slug']}),
+        ('Content body',    {'fields': ['body']}),
+        ('Content miscs',   {'fields': ['author', 'taxonomy']}),
+    ]
+
+admin.site.register(models.Content, ContentAdmin)
 admin.site.register(models.Taxonomy)
-admin.site.register(models.Content)
+
