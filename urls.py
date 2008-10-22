d7dfe888d6f3890b7a0e56ee5b62c67f32e52d46
Kaid Wong <quadfolius@gmail.com>
2008-10-22 23:41:19 +0800

    Pholius 0.1 A1 - URLconf updated [#1 state:open]

--- urls.py
+++ urls.py
@@ -14,4 +14,20 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/(.*)', admin.site.root),
+
+    #date based urls
+    (r'^$', 'pholius.core.views.index'),
+    (r'^(?P<year>\d{4})/$', 'pholius.core.views.year_archive'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{2})/$', 'pholius.core.views.month_archive'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'pholius.core.day_archive'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<title_slug>\w+)/$', 'pholius.core.content_detail'),
+    #taxonomy based urls
+    (r'^(?P<taxonomy_type>\w+)/$', 'pholius.core.views.taxonomies'),
+    (r'^(?P<taxonomy_type>\w+)/(?P<taxnomy_slug>\w+)/$', 'pholius.core.views.taxonomy_archive'),
+    #author based urls
+    (r'^authors/$', 'pholius.core.views.authors'),
+    (r'^authors/(?P<author_slug>\w+)/$', 'pholius.core.views.author'),
+    #static page urls
+    (r'^(?P<page_slug>\w+)/$', 'pholius.core.views.page'),
+
 )
